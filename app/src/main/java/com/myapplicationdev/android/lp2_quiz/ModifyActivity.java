0f62329161a86c9af1ea425de3ed5d113979b8a9
18016375 <18016375@myrp.edu.sg>
2020-05-22 22:48:45 +0800

    activities 1-1000000

--- app/src/main/java/com/myapplicationdev/android/lp2_quiz/ModifyActivity.java
+++ app/src/main/java/com/myapplicationdev/android/lp2_quiz/ModifyActivity.java
@@ -3,12 +3,41 @@ package com.myapplicationdev.android.lp2_quiz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class ModifyActivity extends AppCompatActivity {
+    Button btnUpdate, btnDelete;
+    EditText etContent;
+    ToDo data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_modify);
+
+        btnUpdate = findViewById(R.id.btnUpdate);
+        btnDelete = findViewById(R.id.btnDelete);
+        etContent = findViewById(R.id.etContent);
+
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DBHelper dbh = new DBHelper(ModifyActivity.this);
+                data.setNoteContent(etContent.getText().toString());
+                dbh.updateToDo(data);
+                dbh.close();
+            }
+        });
+
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DBHelper dbh = new DBHelper(ModifyActivity.this);
+                dbh.deleteToDo(data.getId());
+                dbh.close();
+            }
+        });
     }
 }
